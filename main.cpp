288085fb635b83e213c18644999f9afa3286b748
Elijah Omelchenko <elijah.jesus@icloud.com>
2016-12-23 01:58:39 +0200

    step 1.2 normal debugging

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 // #include "baseBinTree.h"
+#include "Windows.h"
+
 using namespace std;  
 int main()  
 { 
-    cout << "Hello WORLD!!!)))"; 
+    int a;
+    char b[10];
+    cin >> a;
+    cout << "a + 3 = " << a + 3 << endl;
+    cout << "press any kay!" << endl;
+    cin >> b;
 }
